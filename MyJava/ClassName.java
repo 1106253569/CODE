e9d26df5aeb6fc7017fae9041986da0d4a66ab95
1106253569 <1106253569@qq.com>
2022-03-18 11:16:03 +0800

    MyJava

---
+++ MyJava/ClassName.java
@@ -0,0 +1,19 @@
+/**
+ * This is a first program in Core Java Chapter3
+ * Let's go!
+ */
+
+public class ClassName
+{
+    public static final double C=2.54;
+    public static void main(String[] args)
+    {
+        int i = 4;
+        int j = 5;
+        System.out.println(i+j);
+        System.out.println("Hello world!");
+        double a=8.5;
+        double b=11;
+        System.out.println("paper size in centimeters:"+a*C+" by "+b*C);
+    }
+}
