89a2b839be7586bca0cc87e0c0570a56e93e8485
1106253569 <1106253569@qq.com>
2022-03-18 22:16:31 +0800

    MyJava

--- MyJava/STRING.java
+++
@@ -1,8 +0,0 @@
-public class STRING {
-    public static void main(String[] args){
-        String w="";
-        String greeting="Hello";
-        String s=greeting.substring(0,3);
-        System.out.println(s);
-    }
-}
