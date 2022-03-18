a7db18ccbd7b5efcec43de52544eb3b896c2dcf3
1106253569 <1106253569@qq.com>
2022-03-18 17:48:59 +0800

    MyJava

--- MyJava/ClassName.java
+++ MyJava/ClassName.java
@@ -3,17 +3,28 @@
  * Let's go!
  */
 
-public class ClassName
-{
-    public static final double C=2.54;
-    public static void main(String[] args)
-    {
+public class ClassName {
+    public static final double C = 2.54;
+
+    public static void main(String[] args) {
         int i = 4;
         int j = 5;
-        System.out.println(i+j);
+        System.out.println(i + j);
         System.out.println("Hello world!");
-        double a=8.5;
-        double b=11;
-        System.out.println("paper size in centimeters:"+a*C+" by "+b*C);
+        double a = 8.5;
+        double b = 11;
+        System.out.println("paper size in centimeters:" + a * C + " by " + b * C);
+        enum Size {S, M, L, XL}
+        Size s = Size.M;
+        System.out.println(s);
+        double x = 4;
+        double y = Math.sqrt(x);
+        System.out.println(y);
+        double z = Math.pow(x, 3);
+        System.out.println(z);
+        x = 9.997;
+        int nx = (int) Math.round(x);
+        System.out.println(nx + (int) x);
+
     }
 }
