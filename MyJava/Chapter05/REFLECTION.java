80c0191beeafb58a37b66eed2b273bc9753d7f77
1106253569 <1106253569@qq.com>
2022-03-21 01:00:59 +0800

    MyJava

--- MyJava/Chapter05/REFLECTION.java
+++ MyJava/Chapter05/REFLECTION.java
@@ -1,7 +1,6 @@
 public class REFLECTION {
     public static void main(String[] args) {
         var e = new Employee("LiSi", 50000, 1988, 3, 6);
-
     }
 
     public static void doSomethingWithClass(String name)
