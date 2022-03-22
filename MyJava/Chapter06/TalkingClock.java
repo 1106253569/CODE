868ed9681224bf7e56b3d7d03828abbe0df2eddf
1106253569 <1106253569@qq.com>
2022-03-22 16:10:04 +0800

    MyJava

---
+++ MyJava/Chapter06/TalkingClock.java
@@ -0,0 +1,7 @@
+public class TalkingClock {
+    private int interval;
+    private boolean beep;
+
+    public TalkingClock(int interval, boolean beep) {
+    }
+}
--- MyJava/Chapter06/TimerTest.java
+++ MyJava/Chapter06/TimerTest.java
@@ -1,15 +1,22 @@
 import javax.swing.*;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.time.Instant;
 
 
 public class TimerTest {
     public static void main(String[] args) {
-        var listener = new TimePrinter();
-
-        var timer = new Timer(1000, (ActionListener) listener);
+        /*var listener = new TimePrinter();
+         *var timer = new Timer(1000, (ActionListener) listener);
+         * */
+        var timer = new Timer(1000, event -> {
+            System.out.println("At the tone, the time is "
+                    + Instant.ofEpochMilli(event.getWhen()));
+            Toolkit.getDefaultToolkit().beep();
+        });
         timer.start();
 
         JOptionPane.showMessageDialog(null, "Quit program");
         System.exit(0);
+
     }
 }
