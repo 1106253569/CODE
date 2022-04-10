aa19f2f41e0fb8d15be7a84277a290fd4b85941f
1106253569 <1106253569@qq.com>
2022-04-10 15:05:32 +0800

    change

--- C++/面向过程/practice/Demo.cpp
+++ C++/面向过程/practice/Demo.cpp
@@ -9,6 +9,17 @@ private:
     double y;
 
 public:
+    Point()
+    {
+        double _x;
+        double _y;
+        std::cout << "please enter x: ";
+        std::cin >> _x;
+        std::cout << "please enter y: ";
+        std::cin >> _y;
+        x = _x;
+        y = _y;
+    }
     Point(double _x, double _y) : x(_x), y(_y) {}
     double getX() { return x; }
     double getY() { return y; }
@@ -16,7 +27,6 @@ public:
 
 double distance1(Point lc, Point rc)
 {
-
     return sqrt(pow(lc.getX() - rc.getX(), 2) + pow(lc.getY() - rc.getY(), 2));
 }
 
@@ -27,8 +37,8 @@ double distance2(Point lc, Point rc)
 
 int main()
 {
-    Point x1(-1, -1);
-    Point x2(1, 1);
+    Point x1;
+    Point x2;
     std::cout << "This is normal functions's result: " << distance1(x1, x2) << '\n';
     std::cout << "This is friend functions's result: " << distance2(x1, x2) << '\n';
     return 0;
