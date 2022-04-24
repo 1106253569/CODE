19d2969dae4cf158e8e91e18656e35ea960ac10f
1106253569 <1106253569@qq.com>
2022-04-24 17:47:56 +0800

    change

--- Matlab/practice/Demo3D.m
+++ Matlab/practice/Demo3D.m
@@ -1,7 +1,16 @@
-clear;
-t=0:0.01:2*pi;
-x=sin(t);
-y=cos(t);
-z=2-2.*y./3;
-grid on;
-plot3(x,y,z)
+%牛顿迭代法
+a=input('a=');
+if a<0
+    warning("a must >0");
+end
+x = a/2; 
+while true
+     x1 = (x+a/x)/2;
+     %前后两次结果之差处于精度范围内则输出
+     if abs(x-x1)<1e-5
+        break;
+     end
+     %最后输出的是x1的值
+     x = x1;  
+end
+disp(x);
--- MySQL/ChapterXI.sql
+++ MySQL/ChapterXI.sql
@@ -1,12 +1,73 @@
-SELECT  c.cust_id, c.fed_id,
-    CASE
-        WHERE c.cust_type_cd='I'
-            THEN concat(i.fname,' ',i.lname)
-        WHEN c.cust_type_cd='B'
-            THEN b.name
+SELECT 
+    c.cust_id,
+    c.fed_id,
+    (CASE
+        WHEN c.cust_type_cd = 'I' THEN CONCAT(i.fname, ' ', i.lname)
+        WHEN c.cust_type_cd = 'B' THEN b.name
         ELSE 'Unknown'
-    END AS name
-FROM customer AS c LEFT OUTER JOIN individual AS i
-    ON c.cust_id=i.cust_id
-    LEFT OUTER JOIN business AS b
-    ON c.cust_id=b.cust_id;
+    END) AS name
+FROM
+    customer AS c
+        LEFT OUTER JOIN
+    individual AS i ON c.cust_id = i.cust_id
+        LEFT OUTER JOIN
+    business AS b ON c.cust_id = b.cust_id;
+    
+SELECT 
+    c.cust_id,
+    c.fed_id,
+    (CASE
+        WHEN
+            c.cust_type_cd = 'I'
+        THEN
+            (SELECT 
+                    CONCAT(i.fname, ' ', i.lname)
+                FROM
+                    individual AS i
+                WHERE
+                    i.cust_id = c.cust_id)
+        WHEN
+            c.cust_type_cd = 'B'
+        THEN
+            (SELECT 
+                    b.name
+                FROM
+                    business AS b
+                WHERE
+                    b.cust_id = c.cust_id)
+        ELSE 'Unknown'
+    END) AS name
+FROM
+    customer AS c;
+
+SELECT 
+    YEAR(open_date) AS year, COUNT(*) AS how_many
+FROM
+    account
+WHERE
+    open_date > '1999-12-31'
+        AND open_date < '2006-01-01'
+GROUP BY YEAR(open_date);
+
+SELECT 
+    CONCAT('ALERT!:Account #',
+            a.account_id,
+            'Has Incorrect Balance!') AS '\'ALERT!:Account #\'+a.account_id+\'Has Incorrect Balance!\''
+FROM
+    account AS a
+WHERE
+    (a.avail_balance , a.pending_balance) <> (SELECT 
+            SUM(CASE
+                    WHEN t.funds_avail_date > CURRENT_TIMESTAMP() THEN 0
+                    WHEN t.txn_type_cd = 'DBT' THEN t.amount * - 1
+                    ELSE t.amount
+                END),
+                SUM(CASE
+                    WHEN t.txn_type_cd = 'DBT' THEN t.amount * - 1
+                    ELSE t.amount
+                END)
+        FROM
+            transaction AS t
+        WHERE
+            t.account_id = a.account_id);
+
