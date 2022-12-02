951662377bc7b12ed2b73ffee172a3b7aa08818e
1106253569 <1106253569@qq.com>
2022-12-02 18:22:03 +0800

    change

--- Python/practice/最大公约数.py
+++ Python/practice/最大公约数.py
@@ -1,19 +1,16 @@
-def compare(a, b):
-    if a < b:
-        return a
-    elif a > b:
-        return b
-    else:
-        return a
+def greatest_common_divisor(a, b):
+    temp = min(a, b)
+    m = 0
+    i = 1
+    while i <= temp:
+        if a % i == 0 and b % i == 0:
+            m = i
+        i += 1
+    return m
 
 
-one, two = eval(input("please enter two numbers:"))
-temp = compare(one, two)
-max = 0
-i = 1
-while i <= temp:
-    if one % i == 0 and two % i == 0:
-        max = i
-    i += 1
-min = (one * two) / max
-print("最大公约数={}\n最小公倍数={}".format(max, min))
+one = eval(input("please enter a :"))
+two = eval(input("please enter b :"))
+_max = greatest_common_divisor(one, two)
+_min = (one * two) / _max
+print("最大公约数={}\n最小公倍数={}".format(_max, _min))
