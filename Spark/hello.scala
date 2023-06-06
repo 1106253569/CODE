6c08b5e935ef5ad9c17de3a1900472a709d16946
1106253569 <1106253569@qq.com>
2023-06-06 14:23:59 +0800

    change

--- Spark/hello.scala
+++ Spark/hello.scala
@@ -1,3 +1,4 @@
+import scala.io.StdIn
 object hello {
   def main(args: Array[String]): Unit = {
     // 常量
@@ -11,5 +12,31 @@ object hello {
     println(word)
     // 字符串模板（插值字符串）:通过$获取变量值
     printf("a=%d\n", a)
+    // 输入
+    printf("please enter name: ")
+    val name: String = StdIn.readLine()
+    printf("please enter age: ")
+    var age: Int = StdIn.readInt()
+    println(s"Welcome ${age} years student ${name}!")
+    if (age >= 18) {
+      for (i <- age to { 18 } by -1) {
+        println(s"${i} BinaryString is ${i.toBinaryString}")
+      }
+    }
+    // 设置50-100之间的五个整数用于测试函数
+    val score = Range(50, 100, step = 10)
+    for (i <- score.reverse) {
+      println(s"$i: ${scoreChange(i)}")
+    }
+  }
+
+  private def scoreChange(score: Double): Char = {
+    val result =
+      if (score >= 90) 'A'
+      else if (score >= 80) 'B'
+      else if (score >= 70) 'C'
+      else if (score >= 60) 'D'
+      else 'E'
+    return result
   }
 }
